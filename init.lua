d9a694e6e55f595ff22c59e0de45dabc04d3f141
foge <mr.texnik666@gmail.com>
2025-04-24 15:47:39 +0300

    keymap

--- init.lua
+++ init.lua
@@ -1,2 +1,3 @@
 require("property")
+require("keymap")
 
---
+++ lua/keymap.lua
@@ -0,0 +1,23 @@
+local function describe(x, desc)
+    return vim.tbl_extend("force", x, { desc = desc })
+end
+
+vim.g.mapleader=' '
+local opts = { noremap = true, silent = true }
+vim.keymap.set('n', '<leader>pv', vim.cmd.Ex, describe(opts, 'Open file manger'))
+vim.keymap.set('i', '"', '""<left>', describe(opts, 'Replace " to ""'))
+vim.keymap.set('i', "'", "''<left>", describe(opts, ""))
+vim.keymap.set('i', '(', '()<left>', describe(opts, ""))
+vim.keymap.set('i', '(;', '();<left><left>', describe(opts, ""))
+vim.keymap.set('i', '(<CR>', '(<CR>)<ESC>O', describe(opts, ""))
+vim.keymap.set('i', '(;<CR>', '(<CR>);<ESC>O', describe(opts, ""))
+vim.keymap.set('i', '{', '{}<left>', describe(opts, ""))
+vim.keymap.set('i', '{;', '{};<left><left>', describe(opts, ""))
+vim.keymap.set('i', '{<CR>', '{<CR>}<ESC>O', describe(opts, ""))
+vim.keymap.set('i', '{;<CR>', '{<CR>};<ESC>O', describe(opts, ""))
+vim.keymap.set('i', '[', '[]<left>', describe(opts, ""))
+vim.keymap.set('i', '[;', '[];<left><left>', describe(opts, ""))
+vim.keymap.set('i', '[<CR>', '[<CR>]<ESC>O', describe(opts, ""))
+vim.keymap.set('i', '[;<CR>', '[<CR>];<ESC>O', describe(opts, ""))
+vim.keymap.set('i', '<C-H>', '<C-w>', describe(opts, "Delete previous word"))
+
