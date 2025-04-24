54d05f8b51afa0f2dcc509bca3e83ec15e9ad54e
foge <mr.texnik666@gmail.com>
2025-04-24 17:13:42 +0300

    gitsigns

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -2,6 +2,7 @@ require("lazy").setup({
     spec = {
         { import = "plugins.gruvbox" },
         { import = "plugins.mason" },
+        { import = "plugins.gitsigns" },
     },
     checker = { enabled = true },
 })
