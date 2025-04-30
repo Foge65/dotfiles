cfb2c4fa3096a45337ab849082f0d2159903194f
foge <mr.texnik666@gmail.com>
2025-04-30 23:40:52 +0300

    jdtls home path over env

--- lua/plugins/lspconfig.lua
+++ lua/plugins/lspconfig.lua
@@ -1,12 +1,13 @@
 return {
     "neovim/nvim-lspconfig",
     config = function()
+        local home = os.getenv("HOME")
         require("lspconfig").jdtls.setup({
             cmd = {
                 'java',
                 '-javaagent:' .. vim.fn.expand('~/.local/share/nvim/mason/packages/jdtls/lombok.jar'),
-                '-jar', vim.fn.glob('/home/user/.local/share/nvim/mason/packages/jdtls/plugins/org.eclipse.equinox.launcher_*.jar'),
-                '-configuration', '/home/user/.local/share/nvim/mason/packages/jdtls/config_linux',
+                '-jar', vim.fn.glob(home .. '/.local/share/nvim/mason/packages/jdtls/plugins/org.eclipse.equinox.launcher_*.jar'),
+                '-configuration', home .. '/.local/share/nvim/mason/packages/jdtls/config_linux',
                 '-data', vim.fn.expand('~/.cache/jdtls-workspace/') .. vim.fn.fnamemodify(vim.fn.getcwd(), ':p:h:t')
             },
         })
