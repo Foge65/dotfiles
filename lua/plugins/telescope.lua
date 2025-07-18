06d00af8f1a73c32ef0e72576ece1ccf21f98cb3
Foge65 <mr.texnik666@gmail.com>
2025-07-18 18:51:28 +0300

    add exclude dirs

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -3,9 +3,23 @@ return {
     tag = "0.1.8",
     dependencies = { "nvim-lua/plenary.nvim" },
     config = function()
+        local telescope = require('telescope')
         local builtin = require('telescope.builtin')
-        vim.keymap.set('n', '<leader>pf', builtin.find_files, {desc = "Telescope find"} )
-        vim.keymap.set('n', '<leader>pb', builtin.buffers, {desc = "Telescope buffer"})
-    end
+
+        telescope.setup {
+            defaults = {
+                file_ignore_patterns = {
+                    "node_modules",
+                    "target",
+                    "%.git/",
+                    "dist",
+                    "build",
+                },
+            },
+        }
+
+        vim.keymap.set('n', '<leader>pf', builtin.find_files, { desc = "Telescope find" })
+        vim.keymap.set('n', '<leader>pb', builtin.buffers,    { desc = "Telescope buffer" })
+    end,
 }
 
