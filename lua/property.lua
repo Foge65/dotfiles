255e2a8c0a1e517251d1cb991ed9de7cd134a2f9
Foge65 <mr.texnik666@gmail.com>
2025-08-13 16:08:45 +0300

    auto save

--- lua/property.lua
+++ lua/property.lua
@@ -15,3 +15,11 @@ vim.diagnostic.config({
 
 vim.opt.ignorecase = true
 
+vim.api.nvim_create_autocmd({ "BufLeave", "FocusLost" }, {
+    callback = function()
+        if vim.bo.modified and not vim.bo.readonly and vim.fn.expand("%") ~= "" and vim.bo.buftype == "" then
+            vim.api.nvim_command('silent update')
+        end
+    end,
+})
+
