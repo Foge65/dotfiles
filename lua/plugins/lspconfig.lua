bdea0c4d1125ef44e78af020ba3ad0b6db635430
Foge65 <mr.texnik666@gmail.com>
2025-08-14 15:36:21 +0300

    lua lsp. auto format text

--- lua/plugins/lspconfig.lua
+++ lua/plugins/lspconfig.lua
@@ -6,34 +6,44 @@ return {
             cmd = {
                 'java',
                 '-javaagent:' .. vim.fn.expand('~/.local/share/nvim/mason/packages/jdtls/lombok.jar'),
-                '-jar', vim.fn.glob(home .. '/.local/share/nvim/mason/packages/jdtls/plugins/org.eclipse.equinox.launcher_*.jar'),
+                '-jar', vim.fn.glob(home ..
+                '/.local/share/nvim/mason/packages/jdtls/plugins/org.eclipse.equinox.launcher_*.jar'),
                 '-configuration', home .. '/.local/share/nvim/mason/packages/jdtls/config_linux',
                 '-data', vim.fn.expand('~/.cache/jdtls-workspace/') .. vim.fn.fnamemodify(vim.fn.getcwd(), ':p:h:t')
             },
             on_attach = function(_, bufnr)
                 local opts = { noremap = true, silent = true, buffer = bufnr }
-                vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts, {desc = "Go to definition"})
-                vim.keymap.set("n", "gi", vim.lsp.buf.implementation, opts, {desc = "Go to implementation"})
-                vim.keymap.set("n", "gr", vim.lsp.buf.references, opts, {desc = "Go to references"})
-                vim.keymap.set("n", "gq", vim.lsp.buf.hover, opts, {desc = "Show describe"})
+                vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts, { desc = "Go to definition" })
+                vim.keymap.set("n", "gi", vim.lsp.buf.implementation, opts, { desc = "Go to implementation" })
+                vim.keymap.set("n", "gr", vim.lsp.buf.references, opts, { desc = "Go to references" })
+                vim.keymap.set("n", "gq", vim.lsp.buf.hover, opts, { desc = "Show describe" })
             end,
         })
 
         require("lspconfig").ts_ls.setup({
             filetypes = { "javascript", "javascriptreact", "typescript", "typescriptreact" },
             root_dir = function(fname)
-                return require("lspconfig.util").root_pattern("package.json", "yarn.lock", "tsconfig.json", ".git")(fname)
+                return require("lspconfig.util").root_pattern("package.json", "yarn.lock", "tsconfig.json", ".git")(
+                        fname)
                     or vim.loop.cwd()
             end,
         })
 
         require('lspconfig').clangd.setup({
-            cmd = {'clangd', '--background-index', '--clang-tidy', '--log=verbose'},
+            cmd = { 'clangd', '--background-index', '--clang-tidy', '--log=verbose' },
             init_options = {
                 fallbackFlags = { '-std=c++17' },
             },
         })
 
+        require('lspconfig').lua_ls.setup({
+            settings = {
+                Lua = {
+                    diagnostics = {
+                        globals = { 'vim' },
+                    },
+                }
+            }
+        })
     end
 }
-
--- lua/property.lua
+++ lua/property.lua
@@ -7,19 +7,20 @@ vim.opt.shiftwidth = 4
 vim.opt.softtabstop = 4
 
 vim.diagnostic.config({
-  virtual_text = {
-    spacing = 4,
-    prefix = "●",
-  },
+    virtual_text = {
+        spacing = 4,
+        prefix = "●",
+    },
 })
 
 vim.opt.ignorecase = true
 
-vim.api.nvim_create_autocmd({ "BufLeave", "FocusLost" }, {
+vim.api.nvim_create_autocmd({ "BufLeave", "FocusLost", "BufWritePre" }, {
+    pattern = "*",
     callback = function()
         if vim.bo.modified and not vim.bo.readonly and vim.fn.expand("%") ~= "" and vim.bo.buftype == "" then
             vim.api.nvim_command('silent update')
+            vim.lsp.buf.format({ async = false })
         end
     end,
 })
-
