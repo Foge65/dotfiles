f1f9463129afc3c439c83681a11a9e53a149214d
Foge65 <mr.texnik666@gmail.com>
2025-08-20 14:22:56 +0300

    enable autoformat

--- lua/property.lua
+++ lua/property.lua
@@ -15,7 +15,7 @@ vim.diagnostic.config({
 
 vim.opt.ignorecase = true
 
-vim.api.nvim_create_autocmd({ "BufLeave", "FocusLost" }, {
+vim.api.nvim_create_autocmd({ "BufLeave", "FocusLost", "BufWritePre" }, {
     pattern = "*",
     callback = function()
         if vim.bo.modified and not vim.bo.readonly and vim.fn.expand("%") ~= "" and vim.bo.buftype == "" then
