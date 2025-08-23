fc3a2fa1a4003bef5628444f24aad3aac5a324b0
Foge65 <mr.texnik666@gmail.com>
2025-08-23 16:13:06 +0300

    add alt+; to insert ; to end line

--- lua/keymap.lua
+++ lua/keymap.lua
@@ -4,6 +4,7 @@ end
 
 vim.g.mapleader = ' '
 local opts = { noremap = true, silent = true }
+vim.keymap.set('n', '<Esc>', ':nohlsearch<CR>', describe(opts, ""))
 vim.keymap.set('n', '<leader>pv', vim.cmd.Ex, describe(opts, 'Open file manger'))
 vim.keymap.set('i', '"', '""<left>', describe(opts, 'Replace " to ""'))
 vim.keymap.set('i', "'", "''<left>", describe(opts, ""))
@@ -20,8 +21,7 @@ vim.keymap.set('i', '[;', '[];<left><left>', describe(opts, ""))
 vim.keymap.set('i', '[<CR>', '[<CR>]<ESC>O', describe(opts, ""))
 vim.keymap.set('i', '[;<CR>', '[<CR>];<ESC>O', describe(opts, ""))
 vim.keymap.set('i', '<C-H>', '<C-w>', describe(opts, "Delete previous word"))
-
-vim.keymap.set('n', '<Esc>', ':nohlsearch<CR>', describe(opts, ""))
+vim.keymap.set('n', '<M-;>', 'A;<Esc>', describe(opts, "Insert ; to end line"))
 
 vim.api.nvim_create_autocmd("FileType", {
     pattern = "qf",
