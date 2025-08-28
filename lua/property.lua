b651bc7e86e508b2df486074be7d5ac221a04629
Foge65 <mr.texnik666@gmail.com>
2025-08-28 11:52:22 +0300

    disable autoformat on exit

--- lua/property.lua
+++ lua/property.lua
@@ -20,7 +20,6 @@ vim.api.nvim_create_autocmd({ "BufLeave", "FocusLost", "BufWritePre" }, {
     callback = function()
         if vim.bo.modified and not vim.bo.readonly and vim.fn.expand("%") ~= "" and vim.bo.buftype == "" then
             vim.api.nvim_command('silent update')
-            vim.lsp.buf.format({ async = false })
         end
     end,
 })
