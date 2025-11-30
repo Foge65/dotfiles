bdd5faf3790e3e018c45a24df961710358ff86fd
Foge65 <mr.texnik666@gmail.com>
2025-11-30 12:57:14 +0300

    tab name

--- lua/property.lua
+++ lua/property.lua
@@ -23,3 +23,29 @@ vim.api.nvim_create_autocmd({ "BufLeave", "FocusLost", "BufWritePre" }, {
         end
     end,
 })
+
+vim.o.tabline = "%!v:lua.TabName()"
+
+function TabName()
+    local s = ""
+    local tabs = vim.api.nvim_list_tabpages()
+    local current = vim.api.nvim_get_current_tabpage()
+
+    for _, tab in ipairs(tabs) do
+        local win = vim.api.nvim_tabpage_get_win(tab)
+        local buf = vim.api.nvim_win_get_buf(win)
+        local name = vim.api.nvim_buf_get_name(buf)
+        local filename = vim.fn.fnamemodify(name, ":t")
+        if filename == "" then
+            filename = "[No Name]"
+        end
+
+        if tab == current then
+            s = s .. "%#TabLineSel# > " .. filename .. " < "
+        else
+            s = s .. "%#TabLine#   " .. filename .. "   "
+        end
+    end
+
+    return s .. "%#TabNameFill#"
+end
